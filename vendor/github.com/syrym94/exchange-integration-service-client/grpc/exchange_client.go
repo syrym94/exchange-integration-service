16b70b5362845569d9552db2b75cdaa299bee413
s.zhaiyk <s.zhaiyk@sumatosoft.com>
2024-06-25 18:31:01 +0500

    /v5/asset/deposit/query-sub-member-address /v5/asset/transfer/query-account-coins-balance /v5/asset/withdraw/query-record /v5/asset/withdraw/withdrawable-amount /v5/asset/withdraw/create

---
+++ vendor/github.com/syrym94/exchange-integration-service-client/grpc/exchange_client.go
@@ -0,0 +1,91 @@
+package grpc
+
+import (
+	"context"
+	"github.com/syrym94/exchange-integration-service-client/proto"
+	"google.golang.org/grpc"
+)
+
+type ExchangeClient struct {
+	client proto.ExchangeServiceClient
+}
+
+func NewExchangeClient(conn *grpc.ClientConn) *ExchangeClient {
+	c := new(ExchangeClient)
+
+	c.client = proto.NewExchangeServiceClient(conn)
+
+	return c
+}
+
+func (ec *ExchangeClient) GetTrades(exchange string) ([]*proto.Trade, error) {
+	req := &proto.GetTradesRequest{Exchange: exchange}
+	res, err := ec.client.GetTrades(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res.Trades, nil
+}
+
+func (ec *ExchangeClient) GetWalletBalance(exchange, accountType string) (*proto.WalletBalanceResponse, error) {
+	req := &proto.GetWalletBalanceRequest{AccountType: accountType, Exchange: exchange}
+	res, err := ec.client.GetWalletBalance(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res.Balance, nil
+}
+
+func (ec *ExchangeClient) StreamTicker(exchange, tickerSymbol string) (*proto.TickerResponse, error) {
+	req := &proto.TickerRequest{Exchange: exchange, TickerSymbol: tickerSymbol}
+	res, err := ec.client.StreamTickerData(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res.Recv()
+}
+
+func (ec *ExchangeClient) GetSubDepositAddress(exchange, coin, chainType, subMemberId string) (*proto.GetSubDepositAddressResponse, error) {
+	req := &proto.GetSubDepositAddressRequest{Exchange: exchange, Coin: coin, ChainType: chainType, SubMemberId: subMemberId}
+	res, err := ec.client.GetSubDepositAddress(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (ec *ExchangeClient) GetAccountCoinsBalance(exchange, memberId, accountType, coin string, withBonus int) (*proto.AccountCoinsBalanceResponse, error) {
+	req := &proto.AccountCoinsBalanceRequest{Exchange: exchange, Coin: coin, MemberId: memberId, AccountType: accountType, WithBonus: int64(withBonus)}
+	res, err := ec.client.GetAccountCoinsBalance(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (ec *ExchangeClient) GetWithdrawalRecords(exchange, coin, withdrawId, cursor string, withdrawType, limit int32, startTime, endTime, withBonus int64) (*proto.WithdrawalRecordsResponse, error) {
+	req := &proto.WithdrawalRecordsRequest{Exchange: exchange, Coin: coin, WithdrawId: withdrawId, Cursor: cursor, WithdrawType: withdrawType, Limit: limit, StartTime: startTime, EndTime: endTime}
+	res, err := ec.client.GetWithdrawalRecords(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (ec *ExchangeClient) GetWithdrawableAmount(exchange, coin string) (*proto.WithdrawableAmountResponse, error) {
+	req := &proto.WithdrawableAmountRequest{Exchange: exchange, Coin: coin}
+	res, err := ec.client.GetWithdrawableAmount(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (ec *ExchangeClient) CreateWithdrawal(exchange, coin, chain, address, tag, accountType, amount string, timestamp int64, forceChain int32) (*proto.CreateWithdrawalResponse, error) {
+	req := &proto.CreateWithdrawalRequest{Exchange: exchange, Coin: coin, Chain: chain, Address: address, Tag: tag, Amount: amount, Timestamp: timestamp, ForceChain: forceChain, AccountType: accountType}
+	res, err := ec.client.CreateWithdrawal(context.Background(), req)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
